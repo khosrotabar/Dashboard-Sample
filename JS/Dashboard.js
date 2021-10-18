0e69cea7f3561217c0c806dba221c8068896c5d4
Mohammad Khosrotabar <92398723+khosrotabar@users.noreply.github.com>
2021-10-18 08:58:39 +0330

    Add files via upload

---
+++ JS/Dashboard.js
@@ -0,0 +1,57 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+    'use strict'
+
+    // Graphs
+    var ctx = document.getElementById('myChart')
+    // eslint-disable-next-line no-unused-vars
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [
+                'Sunday',
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday'
+            ],
+            datasets: [{
+                label: ' sales amount($) ',
+                data: [
+                    15339,
+                    21345,
+                    18483,
+                    24003,
+                    23489,
+                    24092,
+                    12034
+                ],
+                lineTension: 0.2,
+                backgroundColor: 'transparent',
+                borderColor: '#007bff',
+                borderWidth: 4,
+                pointBackgroundColor: '#007bff'
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false
+                    }
+                }]
+            },
+            plugins: {
+                legend: {
+                    display: false,
+                    labels: {
+                        color: 'rgb(255, 99, 132)',
+                    }
+                }
+            }
+        }
+    })
+}())
